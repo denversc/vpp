9ff214131087bc0adbcf9c9d3f5dfac9e5fc5398
Denver Coneybeare <denver.coneybeare@gmail.com>
2011-09-05 22:11:44 -0400

    VppOptions.java added

---
+++ vpp/src/main/java/vpp/VppOptions.java
@@ -0,0 +1,17 @@
+/*
+ * VppOptions.java
+ * By: Denver Coneybeare
+ * Sept 05, 2011
+ *
+ * Copyright 2011 Denver Coneybeare
+ * This file is released under the Apache License Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package vpp;
+
+/**
+ * Container for the options of the Velocity Preprocessor application.
+ */
+public class VppOptions {
+
+}
